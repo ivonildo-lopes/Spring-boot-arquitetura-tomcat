0b0af2928783ea7e695f173fe342310b8d525758
ivonildo-lopes <ivonildo-lopes@gmail.com>
2018-02-15 14:46:44 -0300

    colocando o @service no serviceImpl

--- src/main/java/com/arquitetura/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/arquitetura/service/impl/ClienteServiceImpl.java
@@ -3,14 +3,14 @@ package com.arquitetura.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.arquitetura.model.Cliente;
 import com.arquitetura.repository.ClienteDAO;
 import com.arquitetura.service.ClienteService;
 
 
-@Repository
+@Service
 public class ClienteServiceImpl implements ClienteService {
 	
 	@Autowired
